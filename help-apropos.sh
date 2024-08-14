9af3e17ebc0c9904ca3ea840e507c1b92d14c3c4
wileyhy <84648683+wileyhy@users.noreply.github.com>
2024-08-13 23:54:13 -0700

    Update help-apropos.sh
    Polish the script. Expand abbreviations, shorten lines to ~73 spaces, etc

--- help-apropos.sh
+++ help-apropos.sh
@@ -1,17 +1,15 @@
 #!/bin/bash
 ## help-apropos.sh, version 0.1
-#+   A re-implementation written in bash of `help -s`, which is `apropos`
-#+ for bash's help builtin. Where bash's `help` builtin performs bash's
-#+ internal Pattern Matching syntax, this script accepts awk regular
-#+ expressions. This difference in functionality was an accidental design
-#+ flaw in this script. However, as a result, searching for `alias` will 
-#+ also return `unalias`.
+#+   A re-implementation written in bash 5.2 of `help -s`, which is 
+#+ `apropos` for bash's help builtin. Where bash's `help` builtin 
+#+ performs bash's internal Pattern Matching syntax, this script accepts
+#+ awk regular expressions. This difference in functionality was an 
+#+ accidental design flaw in this script.
 #+   SPDX-FileCopyrightText: 2024 Wiley Young
 #+   SPDX-License-Identifier: GPL-3.0-or-later
 
-## Variables, etc
-#+   A list of search strings, either from the CLI or by default for
-#+ demonstration purposes.
+## Variables, etc: define a list of search strings, either from the CLI  
+#+ or by default for demonstration purposes.
 #set -euxo pipefail
 LC_ALL=C
 if [ $# -ne 0 ]; then
@@ -21,22 +19,22 @@ else
 fi
 FF=~/.bash_help_topics
 
-## Remove dead temp directories: Get a list of directories
+## Remove "dead" temporary directories: get a list of directories
 mapfile -d "" -t dirs < <(find ~ -type d -name '*_mkhelp.sh_*' -print0)
 
 #+ If any are found
 if [ "${#dirs[@]}" -gt 0 ]; then
-	#+ For each dir name
+	#+ For each directory name
 	for DD in "${dirs[@]}"; do
 		#+ Get the embedded value of $$, ie, the PID of the
-		#+ invoking shell, then look to see whether the PID of 
-                #+ the found dir is still active
+		#+ invoking shell, then look to see whether the PID from
+                #+ the found directory is still active
 		AA=${DD##*_}
 		BB=$(ps aux | awk -v dd="$AA" '$2 ~ dd')
 
 		#+ If an active PID is found, then continue to the next
-		#+ found dir, ie, the next loop, or, if the found dir is 
-                #+ not from some active script, then remove said found dir
+		#+ found directory, ie, the next loop; or, remove said 
+		#+ found directory
 		if [ -n "$BB" ]; then
 			continue
 		fi
@@ -48,7 +46,8 @@ fi
 mapfile -d "" -t EE < <(
     find ~ -maxdepth 1 -type f -name "*${FF##*/}*" -print0)
 case ${#EE[@]} in
-	#+ If no files exist, then create one. Create a temporary working directory
+	#+ If no files exist then create one; use a new temporary working 
+	#+ directory
 	0)	CC=$(date | sum | tr -d ' \t')
 		DD="$HOME/.tmp_mkhelp.sh_${CC}_$$"
 		mkdir -p "$DD" || exit "$LINENO"
@@ -60,29 +59,30 @@ case ${#EE[@]} in
 		sort "$DD/list_col-1" "$DD/list_col-2" > "$DD/list_col-0"
 		sed -i 's/[ \t]*$//' "$DD/list_col-0"
 
-		## Create durable file and remove working directory. Note, `:` is
-                #+ a Thompson-style comment - readable when xtrace is enabled.
+		## Create a durable file and remove working directory. Note,
+		#+ `:` is a Thompson-style comment - readable when xtrace 
+		#+ is enabled.
 		mv "$DD/list_col-0" "$FF" || exit "$LINENO"
                 : Topics file created.
 		rm -fr "$DD" || exit "$LINENO"
-		;;
+		;; #
 	#+ If one file exists (Thompson-style comment)
 	1)	: Topics file exists.
-		;;
+		;; #
 	#+ If multiple files exist
 	[2-9]|[1-9][0-9]+)
 		echo Multiple topics files exist. Exiting.
 		ls -la "${EE[@]}"
 		exit "$LINENO"
-		;;
+		;; #
         #+ Catch any errors
 	*)	echo Error. Exiting. 
                 exit "$LINENO"
-		;;
+		;; #
 esac
 
-## Print info from the topics file and exit. (Note, using awk regex rather than 
-#+ bash's pattern matching syntax.) 
+## Print info from the topics file and exit. (Note, using awk regex 
+#+ rather than bash's pattern matching syntax.) 
 for HH in "${strings[@]}"; do
 	awk -v regex="$HH" '$1 ~ regex { print $0 }' "$FF"
 done |
